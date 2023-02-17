ae8059152858bb758681691da297f5f12dd7cba3
Akoumia sevan <sevanakoumia@gmail.com>
2023-02-17 20:49:19 +0000

    Mise en place d'un ORM avec sequelize et CRUD sur les tables Authors et Books

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ app.use(express.json())
 
 // import routes
 const postsRoutes = require('./routers/posts');
+const authorsRoutes = require('./routers/authors');
+const booksRoutes = require('./routers/books');
 
 app.get('/',(req, res, next) => {
     res.status(200).json("API for collect data about eazy training")
 })
 
 app.use('/posts', postsRoutes);
+app.use('/authors', authorsRoutes);
+app.use('/books', booksRoutes);
 
 module.exports = app;
